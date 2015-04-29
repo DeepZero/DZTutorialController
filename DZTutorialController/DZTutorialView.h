66993fe240134bea2cf7ed40aabd88f3b517ebd4
Reiji Fukai <deepzero@icloud.com>
2015-04-29 22:33:04 +0900

    #2 Tutorial View クラスファイルの作成と追加

---
+++ DZTutorialController/DZTutorialView.h
@@ -0,0 +1,13 @@
+//
+//  DZTutorialView.h
+//  DZTutorialController
+//
+//  Created by Reiji Fukai on 4/29/15.
+//  Copyright (c) 2015 Deep Zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZTutorialView : UIView
+
+@end
---
+++ DZTutorialController/DZTutorialView.m
@@ -0,0 +1,21 @@
+//
+//  DZTutorialView.m
+//  DZTutorialController
+//
+//  Created by Reiji Fukai on 4/29/15.
+//  Copyright (c) 2015 Deep Zero. All rights reserved.
+//
+
+#import "DZTutorialView.h"
+
+@implementation DZTutorialView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
